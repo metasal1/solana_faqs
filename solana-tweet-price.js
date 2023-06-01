bd35335146c37f2fdeee6e482ff1cb0cb5b3a180
Salim Karim <imsjk8@gmail.com>
2023-06-02 08:08:31 +1000

    added now

--- solana-tweet-price.js
+++ solana-tweet-price.js
@@ -8,19 +8,26 @@ dotenv.config();
 const cron = process.argv[2] || process.env.CRON_TWEET_PRICE;
 console.log(`${import.meta.url} will run every; ${cron}`);
 
+if (cron === 'now') { tweetTweet(); }
+
 const tweetSchedule = schedule.scheduleJob(import.meta.url, cron, async function () {
+
+    tweetTweet()
+});
+
+const nextJob = schedule?.scheduledJobs[Object.keys(schedule.scheduledJobs)[0]];
+const nextJobName = nextJob?.name;
+const nextJobTime = nextJob?.nextInvocation()?.toString();
+
+console.log('Next Job:', nextJobName);
+console.log('Next Job Time:', nextJobTime);
+
+async function tweetTweet() {
 
     const getSolanaPrice = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd");
     const solanaPrice = await getSolanaPrice.json();
     const price = solanaPrice.solana.usd;
-    const data = `Solana is currently $${price}`;
-
+    const data = `Solana is currently $${(price).toFixed(2)}`;
+    console.log(data);
     tweeter(data);
-});
-
-const nextJob = schedule.scheduledJobs[Object.keys(schedule.scheduledJobs)[0]];
-const nextJobName = nextJob.name;
-const nextJobTime = nextJob.nextInvocation().toString();
-
-console.log('Next Job:', nextJobName);
-console.log('Next Job Time:', nextJobTime);
+}
